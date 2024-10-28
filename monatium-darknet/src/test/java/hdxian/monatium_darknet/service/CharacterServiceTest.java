3da80f6059b026617103eac851642fe157046ece
hdXian <jason123325@gmail.com>
2024-10-28 18:51:47 +0900

    #1 add p6spy dependency, add testcode

--- monatium-darknet/src/test/java/hdxian/monatium_darknet/service/CharacterServiceTest.java
+++ monatium-darknet/src/test/java/hdxian/monatium_darknet/service/CharacterServiceTest.java
@@ -11,6 +11,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -26,9 +28,71 @@ class CharacterServiceTest {
     // 캐릭터 추가
     @Test
     @DisplayName("캐릭터 추가")
-    @Rollback(value = false)
+//    @Rollback(value = false)
     void addCharacter() {
         // given
+        Character erpin = generateCharacter("에르핀");
+
+        // when
+        Long savedId = service.addCharacter(erpin);
+
+        // then
+        Character findCharacter = service.findOne(savedId);
+        Assertions.assertThat(findCharacter.getName()).isEqualTo("에르핀");
+        Assertions.assertThat(findCharacter).isEqualTo(erpin);
+    }
+
+
+    // 이름 검색
+    @Test
+    @DisplayName("이름 검색")
+    @Transactional
+//    @Rollback(value = false)
+    void findName() {
+        // given
+        Character erpin = generateCharacter("에르핀");
+        Character ashur = generateCharacter("에슈르");
+        Character tig = generateCharacter("티그");
+
+        // when
+        Long id_erpin = service.addCharacter(erpin);
+        Long id_ashur = service.addCharacter(ashur);
+        Long id_tig = service.addCharacter(tig);
+
+        // then
+        // "에"를 검색했을 때 "에르핀", "에슈르"가 검색되어야 한다.
+
+        List<Character> findResult = service.findByName("에");
+        Assertions.assertThat(findResult).containsExactly(erpin, ashur);
+    }
+
+    // 전체 캐릭터 검색
+    @Test
+    @DisplayName("전체 캐릭터 검색")
+    @Transactional
+//    @Rollback(value = false)
+    void findAll() {
+        // given
+        Character erpin = generateCharacter("에르핀");
+        Character ashur = generateCharacter("에슈르");
+        Character tig = generateCharacter("티그");
+
+        // when
+        Long id_erpin = service.addCharacter(erpin);
+        Long id_ashur = service.addCharacter(ashur);
+        Long id_tig = service.addCharacter(tig);
+
+        // then
+        // 전체 캐릭터를 검색했을 때 3명의 캐릭터, 그리고 에르핀, 에슈르, 티그가 정확히 포함되어 있어야 한다.
+
+        List<Character> findResult = service.findCharacters();
+        Assertions.assertThat(findResult.size()).isEqualTo(3);
+        Assertions.assertThat(findResult).containsExactly(erpin, ashur, tig);
+    }
+
+
+
+    static Character generateCharacter(String name) {
         CharacterStat stat = new CharacterStat(7, 3, 4);
 
         Attack normalAttack = new Attack();
@@ -58,20 +122,11 @@ class CharacterServiceTest {
 
         CharacterUrl urls = new CharacterUrl("portrait_url", "profile_url", "body_url");
 
-        Character erpin = Character.createCharacter("에르핀", "요정 여왕", "강은애", 3, "엘리아스의 수호자, 등장!", "야채는 싫어",
+        Character erpin = Character.createCharacter(name, "요정 여왕", "강은애", 3, "엘리아스의 수호자, 등장!", "야채는 싫어",
                 "달달한 음식/여왕 직위", Race.FAIRY, Personality.PURE, Role.DEALER, AttackType.MAGICAL, Position.BACK, stat,
                 normalAttack, enhancedAttack, lowSkill, highSkill, urls);
 
-        // when
-        Long savedId = service.addCharacter(erpin);
-
-        // then
-        Character findCharacter = service.findOne(savedId);
-        Assertions.assertThat(findCharacter.getName()).isEqualTo("에르핀");
+        return erpin;
     }
-
-    // 전체 캐릭터 검색
-
-    // 이름 검색
 
 }
