7ccd776f1cd7e8f76594799c736bd99365e5b504
hdXian <jason123325@gmail.com>
2024-10-31 15:47:50 +0900

    #1 Skill 엔티티 생성 메서드 추가

--- monatium-darknet/src/main/java/hdxian/monatium_darknet/domain/Skill.java
+++ monatium-darknet/src/main/java/hdxian/monatium_darknet/domain/Skill.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Entity
@@ -30,6 +31,36 @@ public class Skill {
 
     public void addAttribute(String key, String val) {
         this.attributes.add(new Attribute(key, val));
+    }
+
+    // for JPA Spec (일반 비즈니스 로직에서 사용 x)
+    protected Skill() {
+    }
+
+    // 생성 메서드 (저학년 스킬)
+    public static Skill createLowSkill(String name, String description, String imageUrl, Attribute... attributes) {
+        Skill skill = new Skill();
+        skill.setName(name);
+        skill.setCategory(SkillCategory.LOW);
+        skill.setDescription(description);
+        skill.setCooldown(null);
+        skill.setImageUrl(imageUrl);
+
+        skill.attributes.addAll(Arrays.asList(attributes));
+        return skill;
+    }
+
+    // 생성 메서드 (고학년 스킬)
+    public static Skill createHighSkill(String name, String description, Integer cooldown, String imageUrl, Attribute... attributes) {
+        Skill skill = new Skill();
+        skill.setName(name);
+        skill.setCategory(SkillCategory.HIGH);
+        skill.setDescription(description);
+        skill.setCooldown(cooldown);
+        skill.setImageUrl(imageUrl);
+
+        skill.attributes.addAll(Arrays.asList(attributes));
+        return skill;
     }
 
 }
