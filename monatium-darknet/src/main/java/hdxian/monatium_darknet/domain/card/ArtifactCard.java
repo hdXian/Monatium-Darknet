188afaf3fd5781c00a584fddaa1b4bab9e2b2217
hdXian <jason123325@gmail.com>
2024-10-31 18:30:24 +0900

    #2 Card 하위 엔티티(ArtifactCard, SpellCard)에 생성 메서드 추가

--- monatium-darknet/src/main/java/hdxian/monatium_darknet/domain/card/ArtifactCard.java
+++ monatium-darknet/src/main/java/hdxian/monatium_darknet/domain/card/ArtifactCard.java
@@ -1,10 +1,14 @@
 package hdxian.monatium_darknet.domain.card;
 
+import hdxian.monatium_darknet.domain.Attribute;
 import hdxian.monatium_darknet.domain.Skill;
 import hdxian.monatium_darknet.domain.character.Character;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Entity
 @DiscriminatorValue("A")
@@ -20,5 +24,29 @@ public class ArtifactCard extends Card{
     @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "attachment_skill_id")
     private Skill attachmentSkill; // 애착 아티팩트 스킬
+
+    // for JPA spec (일반 비즈니스 로직에서는 사용 x)
+    protected ArtifactCard() {
+    }
+
+    public static ArtifactCard createArtifactCard(String name, CardGrade cardGrade, String description, String story, Integer cost, String imageUrl,
+                                                  Character character, Skill attachmentSkill, Attribute... attributes)
+    {
+
+        ArtifactCard card = new ArtifactCard();
+        card.setName(name);
+        card.setGrade(cardGrade);
+        card.setDescription(description);
+        card.setStory(story);
+        card.setCost(cost);
+        card.setImageUrl(imageUrl);
+        card.setCharacter(character);
+        card.setAttachmentSkill(attachmentSkill);
+
+        List<Attribute> cardAttributes = card.getAttributes();
+        cardAttributes.addAll(Arrays.asList(attributes));
+
+        return card;
+    }
 
 }
