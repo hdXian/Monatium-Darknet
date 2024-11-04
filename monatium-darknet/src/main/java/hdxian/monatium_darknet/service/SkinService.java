74ddcd5e74e6294397e979b841d29b3e00fc4926
hdXian <jason123325@gmail.com>
2024-11-04 19:19:40 +0900

    #6 SkinService 중복 로직 제거 (스킨 검색)

--- monatium-darknet/src/main/java/hdxian/monatium_darknet/service/SkinService.java
+++ monatium-darknet/src/main/java/hdxian/monatium_darknet/service/SkinService.java
@@ -3,14 +3,12 @@ package hdxian.monatium_darknet.service;
 import hdxian.monatium_darknet.domain.character.Character;
 import hdxian.monatium_darknet.domain.skin.Skin;
 import hdxian.monatium_darknet.domain.skin.SkinCategory;
-import hdxian.monatium_darknet.domain.skin.SkinCategoryMapping;
 import hdxian.monatium_darknet.repository.SkinCategoryRepository;
 import hdxian.monatium_darknet.repository.SkinRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -44,12 +42,8 @@ public class SkinService {
     // 스킨 업데이트
     @Transactional
     public Long updateSkin(Long skinId, SkinDto updateParam) {
-        Optional<Skin> find = skinRepository.findOne(skinId);
-        if (find.isEmpty()) {
-            throw new NoSuchElementException("해당 스킨이 존재하지 않습니다. skinId=" + skinId);
-        }
+        Skin skin = findOneSkin(skinId);
 
-        Skin skin = find.get();
         skin.setName(updateParam.getName());
         skin.setGrade(updateParam.getGrade());
         skin.setDescription(updateParam.getDescription());
@@ -67,12 +61,8 @@ public class SkinService {
     // 카테고리 변경 (이름밖에 없긴함)
     @Transactional
     public Long updateSkinCategory(Long categoryId, String updateName) {
-        Optional<SkinCategory> find = categoryRepository.findOne(categoryId);
-        if (find.isEmpty()) {
-            throw new NoSuchElementException("해당 스킨 카테고리가 존재하지 않습니다. categoryId=" + categoryId);
-        }
+        SkinCategory category = findOneCategory(categoryId);
 
-        SkinCategory category = find.get();
         category.setName(updateName);
 
         return category.getId();
@@ -81,17 +71,8 @@ public class SkinService {
     // 스킨 - 카테고리 관계 추가 (스킨에 카테고리든, 카테고리에 스킨이든 추가 가능)
     @Transactional
     public void linkSkinAndCategory(Long skinId, Long categoryId) {
-        Optional<Skin> findSkin = skinRepository.findOne(skinId);
-        Optional<SkinCategory> findCategory = categoryRepository.findOne(categoryId);
-        if (findSkin.isEmpty()) {
-            throw new NoSuchElementException("해당 스킨이 존재하지 않습니다. skinId=" + skinId);
-        }
-        if (findCategory.isEmpty()) {
-            throw new NoSuchElementException("해당 스킨 카테고리가 존재하지 않습니다. categoryId=" + categoryId);
-        }
-
-        Skin skin = findSkin.get();
-        SkinCategory category = findCategory.get();
+        Skin skin = findOneSkin(skinId);
+        SkinCategory category = findOneCategory(categoryId);
 
         // skin의 addCategory()만 호출 -> 내부에서 양방향 연관관계가 모두 설정되어 한 쪽만 호출해도 됨.
         //** 중요 ** cascade 설정은 skin, category 둘 중 한쪽에만 해줘야 함. 둘다 해주면 양쪽 연관관계를 설정하는 과정에서 mapping이 DB에 두번 insert됨.
@@ -105,17 +86,8 @@ public class SkinService {
     // 스킨-카테고리 간 관계 제거 (스킨 입장에서는 카테고리 제거, 카테고리 입장에서는 스킨 제거)
     @Transactional
     public void unLinkSkinAndCategory(Long skinId, Long categoryId) {
-        Optional<Skin> findSkin = skinRepository.findOne(skinId);
-        Optional<SkinCategory> findCategory = categoryRepository.findOne(categoryId);
-        if (findSkin.isEmpty()) {
-            throw new NoSuchElementException("해당 스킨이 존재하지 않습니다. skinId=" + skinId);
-        }
-        if (findCategory.isEmpty()) {
-            throw new NoSuchElementException("해당 스킨 카테고리가 존재하지 않습니다. categoryId=" + categoryId);
-        }
-
-        Skin skin = findSkin.get();
-        SkinCategory category = findCategory.get();
+        Skin skin = findOneSkin(skinId);
+        SkinCategory category = findOneCategory(categoryId);
 
         skin.removeCategory(category);
 //        category.removeSkin(skin); // 동시 호출 금지
@@ -126,12 +98,8 @@ public class SkinService {
 
     @Transactional
     public void deleteSkin(Long skinId) {
-        Optional<Skin> find = skinRepository.findOne(skinId);
-        if (find.isEmpty()) {
-            throw new NoSuchElementException("해당 스킨이 존재하지 않습니다. skinId=" + skinId);
-        }
+        Skin skin = findOneSkin(skinId);
 
-        Skin skin = find.get();
         // 모든 mapping들을 고아 객체로 만들어 JPA가 자동으로 삭제하도록 함. (skin의 mappings의 orphanRemoval = true)
         List<SkinCategory> categories = findCategoriesBySkin(skinId);
         for (SkinCategory category : categories) {
@@ -142,25 +110,20 @@ public class SkinService {
         Character skinCharacter = skin.getCharacter();
         skinCharacter.removeSkin(skin);
 
-        skinRepository.delete(skinId);
+        skinRepository.delete(skin);
     }
 
     @Transactional
     public void deleteSkinCategory(Long categoryId) {
-        Optional<SkinCategory> find = categoryRepository.findOne(categoryId);
-        if (find.isEmpty()) {
-            throw new NoSuchElementException("해당 스킨 카테고리가 존재하지 않습니다. categoryId=" + categoryId);
-        }
+        SkinCategory categoryToRemove = findOneCategory(categoryId);
 
-        // TODO
         // 모든 스킨과의 연관관계를 제거 (연관관계, mapping 추가를 skin에서만 하기 때문)
-        SkinCategory categoryToRemove = find.get();
         List<Skin> skins = findSkinsByCategory(categoryId);
         for (Skin skin : skins) {
             skin.removeCategory(categoryToRemove);
         }
 
-        categoryRepository.delete(categoryId);
+        categoryRepository.delete(categoryToRemove);
     }
 
     // 스킨 검색
