6c3ffd96f43df53900e6080deb8fb55bc800cc9f
hdXian <jason123325@gmail.com>
2024-11-27 22:42:15 +0900

    #13 7 - 메인 페이지의 동적이지 않은 이미지 소스 경로 지정

--- monatium-darknet/src/main/java/hdxian/monatium_darknet/controller/HomeController.java
+++ monatium-darknet/src/main/java/hdxian/monatium_darknet/controller/HomeController.java
@@ -1,11 +1,18 @@
 package hdxian.monatium_darknet.controller;
 
+import hdxian.monatium_darknet.service.CharacterService;
+import hdxian.monatium_darknet.service.NoticeService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final NoticeService noticeService;
+    private final CharacterService characterService;
 
     @GetMapping("/")
     public String home() {
