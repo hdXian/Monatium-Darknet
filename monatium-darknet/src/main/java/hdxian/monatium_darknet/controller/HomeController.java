16ff36fa4f396a2ff169fca40d7e5309425f96c4
hdXian <jason123325@gmail.com>
2024-11-27 23:49:47 +0900

    #13 8 - 메인페이지 공지사항 목록 불러오도록 구현, 추후 페이징 추가 필요

--- monatium-darknet/src/main/java/hdxian/monatium_darknet/controller/HomeController.java
+++ monatium-darknet/src/main/java/hdxian/monatium_darknet/controller/HomeController.java
@@ -1,11 +1,16 @@
 package hdxian.monatium_darknet.controller;
 
+import hdxian.monatium_darknet.domain.character.Character;
+import hdxian.monatium_darknet.domain.notice.Notice;
 import hdxian.monatium_darknet.service.CharacterService;
 import hdxian.monatium_darknet.service.NoticeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,7 +20,14 @@ public class HomeController {
     private final CharacterService characterService;
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+
+        List<Notice> noticeList = noticeService.findAll();
+        List<Character> characterList = characterService.findCharacters();
+
+        model.addAttribute("noticeList", noticeList);
+        model.addAttribute("characterList", characterList);
+
         return "main/mainPage";
     }
 
