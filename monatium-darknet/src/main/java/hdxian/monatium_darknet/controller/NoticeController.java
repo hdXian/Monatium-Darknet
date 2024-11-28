aded1c0a57eabf593d26950d0ccdbd1c222cd541
hdXian <jason123325@gmail.com>
2024-11-28 17:05:47 +0900

    #13 9 - 공지사항 페이지 실제 공지사항 데이터 뿌리기

--- monatium-darknet/src/main/java/hdxian/monatium_darknet/controller/NoticeController.java
+++ monatium-darknet/src/main/java/hdxian/monatium_darknet/controller/NoticeController.java
@@ -1,10 +1,14 @@
 package hdxian.monatium_darknet.controller;
 
+import hdxian.monatium_darknet.domain.notice.Notice;
 import hdxian.monatium_darknet.service.NoticeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/notices")
@@ -14,7 +18,11 @@ public class NoticeController {
     private final NoticeService noticeService;
 
     @GetMapping
-    public String noticeList() {
+    public String noticeList(Model model) {
+
+        List<Notice> noticeList = noticeService.findAll();
+        model.addAttribute("noticeList", noticeList);
+        
         return "notice/noticeList";
     }
 
