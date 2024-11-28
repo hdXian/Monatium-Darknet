42c665ae68f4f768ddd6da2d6d2a3f3a40552660
hdXian <hyun353500@naver.com>
2024-11-28 19:14:32 +0900

    #13 10 - 공지사항 요소별 썸네일 추가

--- monatium-darknet/src/main/java/hdxian/monatium_darknet/controller/NoticeController.java
+++ monatium-darknet/src/main/java/hdxian/monatium_darknet/controller/NoticeController.java
@@ -1,13 +1,18 @@
 package hdxian.monatium_darknet.controller;
 
 import hdxian.monatium_darknet.domain.notice.Notice;
+import hdxian.monatium_darknet.domain.notice.NoticeCategory;
 import hdxian.monatium_darknet.service.NoticeService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -19,8 +24,8 @@ public class NoticeController {
 
     @GetMapping
     public String noticeList(Model model) {
-
         List<Notice> noticeList = noticeService.findAll();
+
         model.addAttribute("noticeList", noticeList);
         
         return "notice/noticeList";
