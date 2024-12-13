8608ae4454742e5b8786e6fb24cfd942b622936d
hdXian <jason123325@gmail.com>
2024-12-14 00:18:21 +0900

    #23 6 - 관리기능 중 캐릭터 목록 페이지 구성

--- monatium-darknet/src/main/java/hdxian/monatium_darknet/controller/management/character/CharacterMgController.java
+++ monatium-darknet/src/main/java/hdxian/monatium_darknet/controller/management/character/CharacterMgController.java
@@ -1,11 +1,14 @@
 package hdxian.monatium_darknet.controller.management.character;
 
+import hdxian.monatium_darknet.domain.character.Character;
 import hdxian.monatium_darknet.service.CharacterService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/management/characters")
@@ -16,6 +19,9 @@ public class CharacterMgController {
 
     @GetMapping
     public String characterList(Model model) {
+        List<Character> characterList = characterService.findCharacters();
+
+        model.addAttribute("characterList", characterList);
         return "management/character/characterList";
     }
 
