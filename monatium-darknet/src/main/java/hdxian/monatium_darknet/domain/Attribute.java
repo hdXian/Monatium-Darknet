f2102f86f4a3da25be1d2d9488d2768a1649c517
hdXian <jason123325@gmail.com>
2024-12-16 03:34:53 +0900

    #23 25 - Attribute 데이터가 세션에 저장되지 않던 문제 해결(setter 추가)

--- monatium-darknet/src/main/java/hdxian/monatium_darknet/domain/Attribute.java
+++ monatium-darknet/src/main/java/hdxian/monatium_darknet/domain/Attribute.java
@@ -2,11 +2,12 @@ package hdxian.monatium_darknet.domain;
 
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
+import lombok.Setter;
 
 // 스킬이나 공격 등 속성 정보를 매핑하기 위함
 // ex) name: "총 마법 피해", value: "500%"
 @Embeddable
-@Getter
+@Getter @Setter
 public class Attribute {
 
     private String attrName;
