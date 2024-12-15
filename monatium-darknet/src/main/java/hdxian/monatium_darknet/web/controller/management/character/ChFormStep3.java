e71f154e0687203e0687738cd8cef34031c1a5af
hdXian <hyun353500@naver.com>
2024-12-15 18:34:18 +0900

    #23 16 - 캐릭터 추가 페이지 3단계 페이지 중 강화공격 설명 뷰 템플릿 적용

--- monatium-darknet/src/main/java/hdxian/monatium_darknet/web/controller/management/character/ChFormStep3.java
+++ monatium-darknet/src/main/java/hdxian/monatium_darknet/web/controller/management/character/ChFormStep3.java
@@ -4,6 +4,7 @@ import hdxian.monatium_darknet.domain.Attribute;
 import hdxian.monatium_darknet.domain.Skill;
 import hdxian.monatium_darknet.domain.character.*;
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +16,7 @@ public class ChFormStep3 {
     private String normalAttackDescription;
     private List<Attribute> normalAttributes = new ArrayList<>();
 
-    // Getter 재정의
+    // Getter
     public List<Attribute> getNormalAttributes() {
         // 리스트가 비어있다면 빈 Attribute 하나 추가 (뷰 렌더링 목적)
         if (normalAttributes.isEmpty()) {
@@ -28,6 +29,16 @@ public class ChFormStep3 {
     private String enhancedAttackDescription;
     List<Attribute> enhancedAttributes = new ArrayList<>();
 
+    // Getter
+    public List<Attribute> getEnhancedAttributes() {
+        // 리스트가 비어있다면 빈 Attribute 하나 추가 (뷰 렌더링 목적)
+        if (enhancedAttributes.isEmpty()) {
+            enhancedAttributes.add(new Attribute("", ""));
+        }
+        return enhancedAttributes;
+    }
+
+    // 저학년 스킬
     private Skill lowSkill;
     private Skill highSkill;
 
