df6629c1c3edda5d1294a283b174ceda4fbb9014
hdXian <jason123325@gmail.com>
2025-01-02 00:16:09 +0900

    #31 13 - 카드 수정 페이지, 폼 구성

--- monatium-darknet/src/main/java/hdxian/monatium_darknet/web/controller/management/card/CardMgController.java
+++ monatium-darknet/src/main/java/hdxian/monatium_darknet/web/controller/management/card/CardMgController.java
@@ -1,6 +1,8 @@
 package hdxian.monatium_darknet.web.controller.management.card;
 
+import hdxian.monatium_darknet.domain.Skill;
 import hdxian.monatium_darknet.domain.card.ArtifactCard;
+import hdxian.monatium_darknet.domain.card.Card;
 import hdxian.monatium_darknet.domain.card.SpellCard;
 import hdxian.monatium_darknet.domain.character.Character;
 import hdxian.monatium_darknet.file.FileDto;
@@ -57,7 +59,8 @@ public class CardMgController {
 
         model.addAttribute("cardList", cardList);
         model.addAttribute("baseUrl", baseUrl);
-        return "management/cards/spellCardList";
+        model.addAttribute("pageTitle", "스펠 카드 관리");
+        return "management/cards/cardList";
     }
 
     // 아티팩트 카드 리스트
@@ -68,7 +71,8 @@ public class CardMgController {
 
         model.addAttribute("cardList", cardList);
         model.addAttribute("baseUrl", baseUrl);
-        return "management/cards/artifactCardList";
+        model.addAttribute("pageTitle", "아티팩트 카드 관리");
+        return "management/cards/cardList";
     }
 
     // 카드 추가 폼 (스펠, 아티팩트 통합)
@@ -79,7 +83,7 @@ public class CardMgController {
         List<Character> characterList = characterService.findCharacters();
 
         // 세션에 있으면 (임시저장 등으로 리다이렉트) 가져오고, 없으면 (최초 요청) 새로운 디폴트 url 지정
-        String cardImageUrl = getImageUrl(session);
+        String cardImageUrl = getImageUrl(session, imageUrlService.getDefaultThumbnailUrl());
 
         model.addAttribute("cardForm", cardForm);
         model.addAttribute("characterList", characterList);
@@ -88,7 +92,7 @@ public class CardMgController {
         return "management/cards/cardAddForm";
     }
 
-    // 카드 추가 요청 (스펠, 아티팩트 통합)
+    // 카드 추가 요청 처리 (스펠, 아티팩트 통합)
     @PostMapping("/new")
     public String addCard(HttpSession session, @RequestParam("action") String action,
                           @ModelAttribute("cardForm") CardAddForm cardForm, Model model) {
@@ -105,7 +109,7 @@ public class CardMgController {
         saveCardImageToTemp(session, cardForm.getImage());
 
         // 2. 임시경로 이미지에 대한 url을 생성하고 모델에 추가한다.
-        String tempImageUrl = getImageUrl(session);
+        String tempImageUrl = getImageUrl(session, imageUrlService.getDefaultThumbnailUrl());
         model.addAttribute(CARD_IMAGE_URL, tempImageUrl);
 
         // 3. 세션의 폼 데이터를 업데이트한다.
@@ -130,6 +134,112 @@ public class CardMgController {
             return "redirect:/management/cards/new";
         }
 
+    }
+
+    // 카드 수정 폼 (스펠, 아티팩트 통합)
+    @GetMapping("/edit/{cardId}")
+    public String editForm(HttpSession session, @PathVariable("cardId") Long cardId, Model model) {
+
+        CardAddForm cardForm = (CardAddForm) Optional.ofNullable(session.getAttribute(CARD_FORM)).orElse(generateNewCardForm(cardId, model));
+
+        if (cardForm.getCardType() == CardType.SPELL) {
+            String imageUrl = getImageUrl(session, imageUrlService.getSpellCardBaseUrl() + cardId);
+            model.addAttribute(CARD_IMAGE_URL, imageUrl);
+        }
+        else {
+            String imageUrl = getImageUrl(session, imageUrlService.getArtifactCardBaseUrl() + cardId);
+            model.addAttribute(CARD_IMAGE_URL, imageUrl);
+        }
+
+        model.addAttribute("cardId", cardId);
+        model.addAttribute(CARD_FORM, cardForm);
+
+        return "management/cards/cardEditForm";
+    }
+
+    // 카드 수정 요청
+    @PostMapping("/edit/{cardId}")
+    public String edit(HttpSession session, @PathVariable("cardId") Long cardId, @RequestParam("action") String action,
+                       @ModelAttribute("cardForm") CardAddForm cardForm, Model model) {
+
+        log.info("edit cardForm = {}", cardForm);
+
+        // 0. 취소 버튼 클릭 시 세션 데이터를 초기화하고 목록으로 리다이렉트
+        if (action.equals("cancel")) {
+            clearSessionAttributes(session);
+            if (cardForm.getCardType() == CardType.SPELL)
+                return "redirect:/management/cards/spell";
+            else
+                return "redirect:/management/cards/artifact";
+        }
+
+        // 1. 이미지를 임시경로에 저장하고, url을 세션에 업데이트 한다.
+        saveCardImageToTemp(session, cardForm.getImage());
+
+        // 2. 임시경로 이미지에 대한 url을 생성하고 모델에 추가한다.
+        if (cardForm.getCardType() == CardType.SPELL) {
+            String imageUrl = getImageUrl(session, imageUrlService.getSpellCardBaseUrl() + cardId);
+            model.addAttribute(CARD_IMAGE_URL, imageUrl);
+        }
+        else {
+            String imageUrl = getImageUrl(session, imageUrlService.getArtifactCardBaseUrl() + cardId);
+            model.addAttribute(CARD_IMAGE_URL, imageUrl);
+        }
+
+        // 3. 세션의 폼 데이터를 업데이트한다.
+        session.setAttribute(CARD_FORM, cardForm);
+
+        // 4. 완료 버튼을 누른 경우 카드 정보를 저장하고 목록으로 리다이렉트
+
+        return "redirect:/management/cards/edit/" + cardId;
+    }
+
+    private CardAddForm generateNewCardForm(Long cardId, Model model) {
+        Card card = cardService.findOneCard(cardId);
+
+        CardAddForm cardForm = new CardAddForm();
+
+        // 공통 부분 처리
+        cardForm.setGrade(card.getGrade());
+        cardForm.setName(card.getName());
+        cardForm.setDescription(card.getDescription());
+        cardForm.setStory(card.getStory());
+        cardForm.setCost(card.getCost());
+        cardForm.setCardAttributes(card.getAttributes());
+
+        // 카드가 스펠 카드인 경우
+        if (card instanceof SpellCard) {
+            cardForm.setCardType(CardType.SPELL);
+//            String imageUrl = imageUrlService.getSpellCardBaseUrl() + cardId;
+//            model.addAttribute(CARD_IMAGE_URL, imageUrl);
+        }
+        // 카드가 아티팩트 카드인 경우
+        else {
+            // 아티팩트 카드인 경우 애착 사도에 대한 정보 처리 필요
+            cardForm.setCardType(CardType.ARTIFACT);
+//            String imageUrl = imageUrlService.getArtifactCardBaseUrl() + cardId;
+//            model.addAttribute(CARD_IMAGE_URL, imageUrl);
+
+            ArtifactCard artifactCard = (ArtifactCard) card;
+
+            // 애착 사도가 있다면
+            if (artifactCard.getCharacter() != null) {
+                cardForm.setHasAttachment(true);
+
+                cardForm.setCharacterId(artifactCard.getCharacter().getId());
+
+                Skill attachmentSkill = artifactCard.getAttachmentSkill();
+                cardForm.setAttachmentSkillName(attachmentSkill.getName());
+                cardForm.setAttachmentSkillDescription(attachmentSkill.getDescription());
+                cardForm.setAttachmentLv3Description(attachmentSkill.getAttachmentLv3Description());
+                cardForm.setAttachmentAttributes(attachmentSkill.getAttributes());
+            }
+            else {
+                cardForm.setHasAttachment(false); // 기본값이라 필수 동작은 아님
+            }
+
+        }
+        return cardForm;
     }
 
 
@@ -184,8 +294,7 @@ public class CardMgController {
     }
 
     // 세션에 이미지 url이 있으면 해당 url을 리턴, 없으면 디폴트 썸네일 url을 리턴
-    private String getImageUrl(HttpSession session) {
-        String defaultUrl = imageUrlService.getDefaultThumbnailUrl();
+    private String getImageUrl(HttpSession session, String defaultUrl) {
         return (String) Optional.ofNullable(session.getAttribute(CARD_IMAGE_URL)).orElse(defaultUrl);
     }
 
