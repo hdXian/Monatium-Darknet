9b7969f909528eb449a6b1ed5065460b579d68e6
hdXian <jason123325@gmail.com>
2025-01-09 20:09:29 +0900

    #33 4 - 스킨 목록 페이지 초안 작성

--- monatium-darknet/src/main/java/hdxian/monatium_darknet/web/controller/management/skin/SkinMgController.java
+++ monatium-darknet/src/main/java/hdxian/monatium_darknet/web/controller/management/skin/SkinMgController.java
@@ -1,12 +1,14 @@
 package hdxian.monatium_darknet.web.controller.management.skin;
 
 import hdxian.monatium_darknet.domain.skin.Skin;
+import hdxian.monatium_darknet.service.ImageUrlService;
 import hdxian.monatium_darknet.service.SkinService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -19,6 +21,8 @@ public class SkinMgController {
 
     private final SkinService skinService;
 
+    private final ImageUrlService imageUrlService;
+
     // 스킨 목록
     @GetMapping
     public String skinList(Model model) {
@@ -26,6 +30,11 @@ public class SkinMgController {
 
         model.addAttribute("skinList", skinList);
         return "management/skins/skinList";
+    }
+
+    @ModelAttribute("skinBaseUrl")
+    public String skinBaseUrl() {
+        return imageUrlService.getSkinBaseUrl();
     }
 
 
