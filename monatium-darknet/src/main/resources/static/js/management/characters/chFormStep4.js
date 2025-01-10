3d0319d0d2d3194c509d34613535639eedd71ff6
hdXian <jason123325@gmail.com>
2025-01-10 18:10:42 +0900

    #36 캐릭터 추가, 수정 창의 취소 버튼 오작동 문제 해결

--- monatium-darknet/src/main/resources/static/js/management/characters/chFormStep4.js
+++ monatium-darknet/src/main/resources/static/js/management/characters/chFormStep4.js
@@ -29,7 +29,6 @@ document.addEventListener('DOMContentLoaded', function() {
 // 어사이드 여부에 따라 입력 칸 노출, 숨김
 document.addEventListener('DOMContentLoaded', function () {
     const radios = document.getElementsByName('enableAside');
-    const asideToggle = document.getElementById('aside-toggle');
     const asideSection = document.getElementById('asideSection');
 
     if (radios && asideSection) {
@@ -37,25 +36,23 @@ document.addEventListener('DOMContentLoaded', function () {
         const isEnabled = Array.from(radios).find(radio => radio.checked && radio.value === 'true');
         if (isEnabled) {
             asideSection.classList.add('show'); // Bootstrap의 show 클래스 추가
-            asideToggle.style.display = 'block';
+            asideSection.style.display = 'block';
         } else {
             asideSection.classList.remove('show'); // Bootstrap의 show 클래스 제거
-            asideToggle.style.display = 'none';
+            asideSection.style.display = 'none';
         }
 
-        // asideSection.style.display = isEnabled ? 'block' : 'none';
 
         // 라디오 버튼 변경 시 표시/숨기기
         radios.forEach(radio => {
             radio.addEventListener('change', function () {
                 if (radio.value === 'true') {
                     asideSection.classList.add('show'); // show 클래스 추가
-                    asideToggle.style.display = 'block';
+                    asideSection.style.display = 'block';
                 } else {
                     asideSection.classList.remove('show'); // show 클래스 제거
-                    asideToggle.style.display = 'none';
+                    asideSection.style.display = 'none';
                 }
-                // asideSection.style.display = radio.value === 'true' ? 'block' : 'none';
             });
         });
     }
