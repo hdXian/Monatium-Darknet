53fc77815d1d8d31a8feed19b3ce3114ab0310f1
hdXian <jason123325@gmail.com>
2025-01-11 23:34:45 +0900

    #39 Spring Security 관련 빈 이동

--- monatium-darknet/src/main/java/hdxian/monatium_darknet/ApplicationConfig.java
+++ monatium-darknet/src/main/java/hdxian/monatium_darknet/ApplicationConfig.java
@@ -5,8 +5,6 @@ import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -17,11 +15,6 @@ public class ApplicationConfig {
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
         return new JPAQueryFactory(em);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 
 }
--- monatium-darknet/src/main/java/hdxian/monatium_darknet/SecurityConfig.java
+++ monatium-darknet/src/main/java/hdxian/monatium_darknet/SecurityConfig.java
@@ -3,6 +3,8 @@ package hdxian.monatium_darknet;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,15 +19,9 @@ public class SecurityConfig {
         return http.build();
     }
 
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .anyRequest().permitAll() // 모든 요청 허용
-//                .and()
-//                .csrf().disable(); // CSRF 보호 비활성화 (개발 단계에서만 사용)
-//
-//        return http.build();
-//    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
