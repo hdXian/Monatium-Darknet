2028ba6f6f7373a3499061c474ac560e3f817cd2
hdXian <jason123325@gmail.com>
2025-01-14 00:16:40 +0900

    #39 12 - skin의 getMapping() 메서드에 대해 불변 리스트를 리턴하도록 변경, 그 외 몇 가지 TODO 처리

--- monatium-darknet/src/main/java/hdxian/monatium_darknet/domain/skin/Skin.java
+++ monatium-darknet/src/main/java/hdxian/monatium_darknet/domain/skin/Skin.java
@@ -5,10 +5,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 @Getter @Setter // Setter는 추후 생성 메서드 등으로 대체해야 함
@@ -37,6 +34,11 @@ public class Skin {
     @OneToMany(mappedBy = "skin", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<SkinCategoryMapping> mappings = new ArrayList<>();
 
+    // 매핑 리스트를 불변 리스트로 리턴
+    public List<SkinCategoryMapping> getMappings() {
+        return Collections.unmodifiableList(this.mappings);
+    }
+
     // 연관관계 메서드 - SkinCategory의 addSkin() 쪽에서도 이걸 호출해야 돼서 public으로 따로 열어놔야 함.
     public void addMapping(SkinCategoryMapping mapping) {
         if (mapping == null) {
@@ -49,7 +51,8 @@ public class Skin {
     protected Skin() {
     }
 
-    // 생성 메서드
+
+    // === 생성 메서드 ===
     public static Skin createSkin(String name, String description, Character character) {
         Skin skin = new Skin();
         skin.setName(name);
@@ -61,7 +64,8 @@ public class Skin {
         return skin;
     }
 
-    // 비즈니스 로직
+
+    // === 비즈니스 로직 ===
     // mapping 생성(내부적으로 skin, category 지정) -> 자신의 mappings에 mapping 추가 -> Category의 mappings에 mapping 추가
     // 이 mapping이 skin에 추가되면 DB에 반영됨 (csacade), 트랜잭션 외부에서 이 메서드를 호출하는 것은 무의미함.
     public void addCategory(SkinCategory category) {
@@ -93,10 +97,5 @@ public class Skin {
         }
 
     }
-
-    // TODO - getMappings() 메서드에 대해 불변 리스트를 리턴하도록 개선 고려
-    // getMappings()의 리턴 타입을 불변 리스트로 설정해도 Skin이나 SkinCategory 등 엔티티 내부의 비즈니스 로직에서는 접근 가능함.
-
-    // TODO - equals, hashCode 추가 고려
 
 }
