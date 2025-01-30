ac4fd9a947bf300955eccf743f6b8b94a8cdbf2c
hdXian <jason123325@gmail.com>
2025-01-30 18:57:46 +0900

    #48 14 - 상단 네비게이션 바 인라인 스크립트 제거

---
+++ monatium-darknet/src/main/resources/static/js/management/langChange.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("languageSelectMg").addEventListener("change", function() {
+        this.form.submit();
+    });
+});
