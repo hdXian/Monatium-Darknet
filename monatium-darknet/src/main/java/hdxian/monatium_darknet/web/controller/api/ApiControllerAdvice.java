4472ee0b28822a004d3b73bcc3b37dfb664497d0
hdXian <jason123325@gmail.com>
2025-02-01 01:45:25 +0900

    #48 23 - (관리 페이지) 캐릭터 정보 미리보기 페이지 css, js 분리, api 에러코드 404 추가

--- monatium-darknet/src/main/java/hdxian/monatium_darknet/web/controller/api/ApiControllerAdvice.java
+++ monatium-darknet/src/main/java/hdxian/monatium_darknet/web/controller/api/ApiControllerAdvice.java
@@ -1,12 +1,15 @@
 package hdxian.monatium_darknet.web.controller.api;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
@@ -24,6 +27,19 @@ public class ApiControllerAdvice {
 //        errorDetails.put("details", ex.getMessage());
 
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // 없는 파일 404로 뱉기
+    @ExceptionHandler(FileNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleFNFE(FileNotFoundException ex, HttpServletRequest request) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("status", HttpStatus.NOT_FOUND.value());
+        errorResponse.put("error", "Not Found");
+        errorResponse.put("message", "resource not found"); // 예외 메시지를 그대로 반환
+        errorResponse.put("path", request.getRequestURI()); // 요청된 경로 포함
+        errorResponse.put("timestamp", Instant.now().toString()); // 현재 시간 추가
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 
 }
