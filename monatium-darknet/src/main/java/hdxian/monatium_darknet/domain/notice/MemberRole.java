0b018b7db24dac9f518e9e9089721fcd499e3e10
hdXian <jason123325@gmail.com>
2025-02-04 19:19:53 +0900

    #52 4 - 사용자 목록, 활성화/비활성화 기능 추가

--- monatium-darknet/src/main/java/hdxian/monatium_darknet/domain/notice/MemberRole.java
+++ monatium-darknet/src/main/java/hdxian/monatium_darknet/domain/notice/MemberRole.java
@@ -1,9 +1,20 @@
 package hdxian.monatium_darknet.domain.notice;
 
 // 최고 관리자, 그냥 관리자
+
+import lombok.Getter;
+
 /**
  ** 중요 ** Spring Security User의 Role로 활용됨.
   */
+@Getter
 public enum MemberRole {
-    SUPER, NORMAL
+    SUPER("최고 관리자"), NORMAL("그냥 관리자");
+
+    private final String name;
+
+    MemberRole(String name) {
+        this.name = name;
+    }
+
 }
--- monatium-darknet/src/main/java/hdxian/monatium_darknet/web/controller/management/member/MemberMgController.java
+++ monatium-darknet/src/main/java/hdxian/monatium_darknet/web/controller/management/member/MemberMgController.java
@@ -1,16 +1,23 @@
 package hdxian.monatium_darknet.web.controller.management.member;
 
 import hdxian.monatium_darknet.domain.LangCode;
+import hdxian.monatium_darknet.domain.notice.Member;
 import hdxian.monatium_darknet.domain.notice.MemberRole;
+import hdxian.monatium_darknet.security.CustomUserDetails;
 import hdxian.monatium_darknet.service.MemberService;
 import hdxian.monatium_darknet.service.dto.MemberDto;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.List;
 import java.util.Optional;
 
 import static hdxian.monatium_darknet.web.controller.management.SessionConst.CURRENT_LANG_CODE;
@@ -29,11 +36,30 @@ public class MemberMgController {
         return Optional.ofNullable((LangCode)session.getAttribute(CURRENT_LANG_CODE)).orElse(LangCode.KO);
     }
 
+    // 회원 목록
     @GetMapping
-    public String memberList() {
+    public String memberList(@AuthenticationPrincipal CustomUserDetails userDetails, Model model) {
+        Long curUserId = userDetails.getMember().getId();
+        List<Member> memberList = memberService.findAll();
+        memberList.removeIf(member -> member.getId().equals(curUserId)); // 현재 로그인한 관리자 계정은 제외
+
+        model.addAttribute("memberList", memberList);
         return "management/members/memberList";
     }
 
+    @PostMapping("/activate/{memberId}")
+    public ResponseEntity<Void> activate(@PathVariable("memberId") Long memberId) {
+        memberService.activateMember(memberId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/deactivate/{memberId}")
+    public ResponseEntity<Void> deactivate(@PathVariable("memberId") Long memberId) {
+        memberService.deactivateMember(memberId);
+        return ResponseEntity.ok().build();
+    }
+
+    // 회원 추가
     @GetMapping("/new")
     public String memberForm(@ModelAttribute("memberForm") MemberForm memberForm) {
         return "management/members/addMemberForm";
@@ -45,6 +71,7 @@ public class MemberMgController {
         MemberDto memberDto = generateMemberDto(memberForm);
 
         Long savedId = memberService.createNewMember(memberDto);
+        // 리다이렉트 후에도 일회성으로 데이터를 저장해놓으려면
         redirectAttributes.addFlashAttribute("message", "가입 신청이 완료되었습니다. 관리자 승인 이후 로그인하실 수 있습니다.");
         return "redirect:/management/login";
     }
